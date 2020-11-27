87756cc3cf0936dbf78cfc2b0ee5d4344a321ac4
CamClrt <camille.clarret@gmail.com>
2020-11-27 15:19:16 +0100

    Prepared for deployment

--- pur_beurre/pur_beurre/settings.py
+++ pur_beurre/pur_beurre/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
-import django_heroku
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -161,4 +160,4 @@ LOGIN_URL = "login"
 AUTH_USER_MODEL = "users.User"
 
 # Activate Django-Heroku.
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
