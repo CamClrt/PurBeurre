05462a917df6ed588c79be86febc33d39b084950
CamClrt <camille.clarret@gmail.com>
2020-11-27 15:53:59 +0100

    Prepared for deployment

--- pur_beurre/pur_beurre/settings.py
+++ pur_beurre/pur_beurre/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = False if os.environ.get("ENV", "development") == "production" else True
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     "localhost",
