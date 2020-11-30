503d808bda622642a7c8cd20e8d3aaff99239482
CamClrt <camille.clarret@gmail.com>
2020-11-30 11:59:34 +0100

    Prepared for deployment

--- pur_beurre/pur_beurre/settings.py
+++ pur_beurre/pur_beurre/settings.py
@@ -81,7 +81,7 @@ else:
         "django.contrib.auth.middleware.AuthenticationMiddleware",
         "django.contrib.messages.middleware.MessageMiddleware",
         "django.middleware.clickjacking.XFrameOptionsMiddleware",
-        "whitenoise.middleware.WhiteNoiseMiddleware",
+        # "whitenoise.middleware.WhiteNoiseMiddleware",
     ]
 
 ROOT_URLCONF = "pur_beurre.urls"
@@ -168,9 +168,9 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 
-# STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 CRISPY_TEMPLATE_PACK = "bootstrap4"
 
